735738cee3471d2bdbe760fd77939e18db65fa8e
Shadi Jumaa <shadijumaa92@gmail.com>
2025-07-04 13:19:09 +0300

    Initial commit

---
+++ src/main/java/com/carBE/demo/CarsAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.carBE.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsAppBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carBE/demo/CarsAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carBE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
